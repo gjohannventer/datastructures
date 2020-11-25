2d3944a9a4f8db0dd5b2df4a05ecf0bce74168ae
aonixe <g.johannventer@gmail.com>
2020-11-25 13:59:41 +0200

    added reverse queue initial

---
+++ Queue/ReversedQueue.py
@@ -0,0 +1,98 @@
+class LinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.num_elements = 0
+        self.head = None
+    
+    def push(self, value):
+        new_node = LinkedListNode(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+           new_node.next = self.head
+           self.head = new_node
+        self.num_elements += 1
+
+    def pop(self):
+        if self.head is None:
+            return None
+        value = self.head.data
+        self.head = self.head.next
+        self.num_elements -= 1
+        return value 
+
+    def top(self):
+        if self.head is None:
+            return None
+        return self.head.data
+
+    def size(self):
+        return self.num_elements
+
+    def is_empty(self):
+        return self.size() == 0
+
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.num_elements = 0
+
+    def enqueue(self, value)
+        new_node = LinkedListNode(data)
+        if self.head is None
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.num_elements += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+
+        value = self.head.data
+        self.head = self.head.next
+        self.num_elements -= 1
+        return value
+
+
+    def front(self):
+        if self.head is None:
+            return None:
+        return self.head.data
+
+    def size(self):
+        return self.num_elements
+
+    def is_empty(self):
+        return self.size() == 0
+
+    def reverse_queue(queue):
+
+def test_function(test_case):
+    queue = Queue()
+    for num in test_case:
+        queue.enqueue(num)
+    
+    reverse_queue(queue)
+    index = len(test_case) - 1
+    while not queue.is_empty():
+        removed = queue.dequeue()
+        if removed != test_case[index]:
+            print("Fail")
+            return
+        else:
+            index -= 1
+    print("Pass")
+
+        
+
+
+
